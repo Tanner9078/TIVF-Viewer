ed6045926d923aa6c70481ec7aefd2676c040523
Max Correia <maxcorreia@gmail.com>
2021-05-28 21:56:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,43 +15,42 @@ int main(int argc, char **argv) {
         std::cin >> filename;
     }
     std::ifstream fileObject(filename);
-
-    std::ostringstream ImgData;  // The data of the Image will go to this string
-    fileObject>>ImgData.rdbuf();  // Put the data of the image file into ImgData
-    int len=ImgData.str().length();  // get ImgData as a string and put the length of Imgdata into a var
+    std::ostringstream ImgData;  // Image data goes to string
+    fileObject>>ImgData.rdbuf();  // File data goes into ImgData
+    int len=ImgData.str().length();  // Get ImgData as a string, put the length of ImgData into a var
     for(int i=0; i<len; i++) {
-        switch(ImgData.str()[i]) { // get the string representation
+        switch(ImgData.str()[i]) { // Get string representation
         case '0':
-              cout << "\033[30m\u2587";  // if image has 0. Make a black square
+              cout << "\033[30m\u2587";  // Black square
               break;
         case 'R':
-              cout << "\033[31m\u2587";  // if image has R. Make a red square
+              cout << "\033[31m\u2587";  // Red square
               break;
         case 'G':
-              cout << "\033[32m\u2587";  // if image has G. Make a green square
+              cout << "\033[32m\u2587";  // Green square
               break;
         case 'O':
-              cout << "\033[33m\u2587";  // if image has O. Make an Orange square
+              cout << "\033[33m\u2587";  // Orange square
               break;
         case 'B':
-              cout << "\033[34m\u2587";  // if image has B. Make a blue square
+              cout << "\033[34m\u2587";  // Blue square
               break;
         case 'M':
-              cout << "\033[35m\u2587";  // if image has M. Make a Magenta square
+              cout << "\033[35m\u2587";  // Magenta square
               break;
         case 'C':
-              cout << "\033[36m\u2587";  // if image has C. Make a Cyan square
+              cout << "\033[36m\u2587";  // Cyan square
               break;
         case 'L':
-              cout << "\033[37m\u2587";  // if image has L. Make a Light gray square
+              cout << "\033[37m\u2587";  // Light gray square
               break;
         case 'D':
-              cout << "\033[39m\u2587";  // if image has D. Use the default color
+              cout << "\033[39m\u2587";  // Default color
               break;
         case '\n':
               break;
         case 'N':
-              cout << "\n";  // if image has N. Make a Newline
+              cout << "\n";  // if image has N. make a new line
               break;
         default:
               cerr << "Undefined character!";
